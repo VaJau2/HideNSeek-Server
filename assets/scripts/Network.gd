f94a129f49217ecfb404a2bf4dc3723a4f5e5e62
VaJau2 <vadim206-1998@yandex.ru>
2022-06-22 18:28:27 +0500

    sync hiding and searching in props

--- assets/scripts/Network.gd
+++ assets/scripts/Network.gd
@@ -22,19 +22,35 @@ remote func add_message_to_chat_from_user(name, message):
 	rpc("add_message_to_chat", name, message)
 
 
+func get_player_data(player_id):
+	var player = playersList.get_player(player_id)
+	return {
+		"id": player_id,
+		"position": player.position,
+		"flip_x": player.flip_x,
+		"gender": player.gender,
+		"state": player.state,
+		"is_hiding": player.is_hiding,
+		"hiding_animation": player.hiding_animation,
+		"my_prop_path": player.my_prop_path,
+		"parts_data": player.partsData
+	}
+
+
 remote func spawn_player_in_map(player_id, position, flip_x, partsData):
 	var player = playersList.get_player(player_id)
 	player.position = position
 	player.flip_x = flip_x
 	player.partsData = partsData
-	rpc("spawn_puppet", player_id, position, flip_x, partsData)
+	
+	rpc("spawn_puppet", get_player_data(player_id))
 
 
 remote func get_puppets(player_id):
 	for key in playersList.playersData.keys():
 		if key == player_id: continue
 		var player = playersList.get_player(key)
-		rpc_id(player_id, "spawn_puppet", key, player.position, player.flip_x, player.partsData)
+		rpc_id(player_id, "spawn_puppet", get_player_data(key))
 
 
 remote func sync_player_movement(data):
@@ -69,8 +85,20 @@ remote func sync_state(player_id, new_state):
 	rpc("sync_state", player_id, new_state)
 
 
+remote func sync_hiding(player_id, hide_on, animation):
+	var player = playersList.get_player(player_id)
+	player.is_hiding = hide_on
+	player.hiding_animation = animation
+	rpc("sync_hiding", player_id, hide_on, animation)
+
+
 remote func start_game(player_id):
 	if gameManager != null: return
 	gameManager = GameManager.new(self, playersList, player_id)
 	if !gameManager.teleport_players(): return
 	gameManager.wait_and_start()
+
+
+remote func save_hide_in_prop(player_id, is_hiding, prop_path):
+	var player = playersList.get_player(player_id)
+	player.my_prop_path = prop_path if is_hiding else ""
--- assets/scripts/PlayerData.gd
+++ assets/scripts/PlayerData.gd
@@ -6,6 +6,9 @@ var name: String
 var gender: String
 var position: Vector2
 var flip_x: bool
+var is_hiding: bool
+var hiding_animation: String
+var my_prop_path: String
 var partsData: Dictionary
 
 enum states {none, wait, search, hide}
@@ -17,3 +20,5 @@ func _init(id: int, name: String, gender: String):
 	self.gender = gender
 	position = Vector2.ZERO
 	timestamp = 0
+	hiding_animation = ""
+	my_prop_path = ""
