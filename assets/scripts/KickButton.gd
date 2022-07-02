c533f5d78dbe3db8d7bdb53749720372df688789
VaJau2 <vadim206-1998@yandex.ru>
2022-07-02 14:11:01 +0500

    bug fix

--- assets/scripts/KickButton.gd
+++ assets/scripts/KickButton.gd
@@ -4,6 +4,7 @@ extends Button
 func on_pressed():
 	var players_list = get_node("../playersList")
 	var player_data = players_list.get_selected_player()
+	players_list.text = ""
 	if player_data:
 		get_node("/root/Main/Network").kick_player(player_data.id)
 	
